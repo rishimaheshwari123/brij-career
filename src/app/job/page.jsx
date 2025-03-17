73dff750da4305690a9842a867120273dc8e80b3
Rishi <rishimaheshwari040@gmail.com>
2025-03-17 13:54:23 +0530

    update job

--- src/app/job/page.jsx
+++ src/app/job/page.jsx
@@ -28,6 +28,11 @@ const JobsPage = () => {
     setExpandedJob(expandedJob === id ? null : id);
   };
 
+  const formatDate = (dateString) => {
+    const options = { year: "numeric", month: "long", day: "numeric" };
+    return new Date(dateString).toLocaleDateString("en-IN", options);
+  };
+
   return (
     <div className="max-w-7xl mx-auto px-6 py-12 mt-16">
       <h2 className="text-3xl font-bold text-center mb-8 text-gray-800">
@@ -48,9 +53,14 @@ const JobsPage = () => {
                 className="flex justify-between items-center p-6 cursor-pointer"
                 onClick={() => toggleJobDetails(job._id)}
               >
-                <h3 className="text-xl font-semibold text-gray-800">
-                  {job.title}
-                </h3>
+                <div>
+                  <h3 className="text-xl font-semibold text-gray-800">
+                    {job.title}
+                  </h3>
+                  <p className="text-sm text-gray-500">
+                    Posted on: {formatDate(job.createdAt)}
+                  </p>
+                </div>
                 <button className="text-gray-600 hover:text-gray-900">
                   {expandedJob === job._id ? <FaMinus /> : <FaPlus />}
                 </button>
@@ -62,7 +72,7 @@ const JobsPage = () => {
                     {job.desc.split(".").map((sentence, index) => (
                       <span key={index}>
                         {sentence.trim()}
-                        {index !== job.desc.split(".").length - 1 && <br />}
+                        {index !== job.desc.split("..").length - 1 && <br />}
                       </span>
                     ))}
                   </p>
